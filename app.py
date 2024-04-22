185261bfb849f43257262fb136384cd9d7909595
Jaxon Avena <jaxonavena@gmail.com>
2024-04-21 20:32:20 -0500

    Got users table up and running

--- app.py
+++ app.py
@@ -7,44 +7,42 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def index():
-	return render_template('index.html')
+  return render_template('index.html')
 
 
 connect = sqlite3.connect('database.db')
-connect.execute(
-	'CREATE TABLE IF NOT EXISTS PARTICIPANTS (name TEXT, \
-	email TEXT, city TEXT, country TEXT, phone TEXT)')
-
+connect.execute('CREATE TABLE IF NOT EXISTS USERS (ID INTEGER PRIMARY KEY, name TEXT, email TEXT)')
+connect.execute('CREATE TABLE IF NOT EXISTS EXPENSE (ID INTEGER PRIMARY KEY, user_id INTEGER, description TEXT, date TEXT, amount REAL, category TEXT, FOREIGN KEY(user_id) REFERENCES USERS(ID))')
+connect.execute('CREATE TABLE IF NOT EXISTS GOAL (ID INTEGER PRIMARY KEY, user_id INTEGER, title TEXT, description TEXT, target_amount REAL, start_time TEXT, end_time TEXT, progress_level TEXT, priority_level TEXT, frequency TEXT, FOREIGN KEY(user_id) REFERENCES USERS(ID))')
 
 @app.route('/join', methods=['GET', 'POST'])
 def join():
-	if request.method == 'POST':
-		name = request.form['name']
-		email = request.form['email']
-		city = request.form['city']
-		country = request.form['country']
-		phone = request.form['phone']
+  if request.method == 'POST':
+    with sqlite3.connect("database.db") as connect:
 
-		with sqlite3.connect("database.db") as users:
-			cursor = users.cursor()
-			cursor.execute("INSERT INTO PARTICIPANTS \
-			(name,email,city,country,phone) VALUES (?,?,?,?,?)",
-						(name, email, city, country, phone))
-			users.commit()
-		return render_template("index.html")
-	else:
-		return render_template('join.html')
+      user_id_tuple = connect.execute("SELECT CASE WHEN MAX(ID) IS NULL THEN 1 ELSE MAX(ID) + 1 END AS next_id FROM USERS")
+      user_id = user_id_tuple.fetchone()[0]
+
+      name = request.form['name']
+      email = request.form['email']
+
+      cursor = connect.cursor()
+      cursor.execute("INSERT INTO USERS (id,name,email) VALUES (?,?,?)", (user_id, name, email))
+      connect.commit()
+    return render_template("index.html")
+  else:
+    return render_template('join.html')
 
 
-@app.route('/participants')
-def participants():
-	connect = sqlite3.connect('database.db')
-	cursor = connect.cursor()
-	cursor.execute('SELECT * FROM PARTICIPANTS')
+@app.route('/users')
+def users():
+  connect = sqlite3.connect('database.db')
+  cursor = connect.cursor()
+  cursor.execute('SELECT * FROM USERS')
 
-	data = cursor.fetchall()
-	return render_template("participants.html", data=data)
+  data = cursor.fetchall()
+  return render_template("users.html", data=data)
 
 
 if __name__ == '__main__':
-	app.run(debug=False)
+  app.run(debug=False)
